1cd2919399d8078804a1330bef56eea587d295f6
Jawwad Bin Zahir <30545989+JawwadSeemanta@users.noreply.github.com>
2022-01-08 00:58:55 +0600

    Initial Commit

---
+++ Code.py
@@ -0,0 +1,35 @@
+# Load Input file
+input_file = open(r".\Timestamps.txt", "r")
+
+# Create if not exists, clear and then write
+output_file = open(r".\Test.pbf", "a+")
+output_file.close() 
+output_file = open(r".\Test.pbf", "r+")
+
+# Write to file
+full_line = []
+count = 0
+lines = input_file.readlines()
+
+output_file.write("[Bookmark]" + '\n')
+
+for line in lines:
+    split_lines = line.split('-', 2)
+    
+    # Get the time and convert to miliseconds
+    time_part = split_lines[0].split(':')
+    timestring = ((int(time_part[0]) * 3600) + (int(time_part[1]) * 60) + (int(time_part[2]))) * 1000
+    
+    # Get the file names
+    name_part = split_lines[1]
+    
+    # Construct the line string
+    full_line = str(count) + "=" + str(timestring) + "*" + name_part.rstrip().lstrip() + "*" + '\n'
+    output_file.write(full_line)
+    
+    count = count + 1
+
+
+
+input_file.close()
+output_file.close()
---
+++ Readme.txt
@@ -0,0 +1,6 @@
+1. Copy the timestamps from youtube description
+2. Run the script
+3. Copy the .pbf file to the video directory.
+4. Rename it to the file name.
+5. Open the file with PotPlayer.
+5. Done!!!
