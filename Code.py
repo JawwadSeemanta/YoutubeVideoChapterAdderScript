69335f47b05c013a97b3b56de6045b257e69dce5
JawwadSeemanta <30545989+JawwadSeemanta@users.noreply.github.com>
2022-01-11 15:02:22 +0600

    Normalize Times Automatically

--- Code.py
+++ Code.py
@@ -1,35 +1,39 @@
 # Load Input file
 input_file = open(r".\Timestamps.txt", "r")
 
-# Create if not exists, clear and then write
+#Create if not exists, clear and then write
 output_file = open(r".\Timestamps.pbf", "a+")
 output_file.close() 
 output_file = open(r".\Timestamps.pbf", "r+")
 
-# Write to file
+# Initialize
 full_line = []
 count = 0
-lines = input_file.readlines()
-
 output_file.write("[Bookmark]" + '\n')
 
-for line in lines:
+for line in input_file.readlines():
     split_lines = line.split('-', 2)
     
     # Get the time and convert to miliseconds
     time_part = split_lines[0].split(':')
-    timestring = ((int(time_part[0]) * 3600) + (int(time_part[1]) * 60) + (int(time_part[2]))) * 1000
     
-    # Get the file names
-    name_part = split_lines[1]
+    if(len(time_part)) < 3 :            # 3 == HH:MM:SS
+        length = len(time_part)
+        for i in range((3 - length)):
+            time_part = [0] + time_part
+    
+    timestring = str(((int(time_part[0]) * 3600) + (int(time_part[1]) * 60) + (int(time_part[2]))) * 1000)
+    
+    
+    # Get the file name
+    name_part = split_lines[1].rstrip().lstrip()
+    
     
     # Construct the line string
-    full_line = str(count) + "=" + str(timestring) + "*" + name_part.rstrip().lstrip() + "*" + '\n'
+    full_line = str(count) + "=" + str(timestring) + "*" + name_part + "*" + '\n'
     output_file.write(full_line)
     
     count = count + 1
-
-
 
 input_file.close()
 output_file.close()
